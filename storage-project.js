b6db3c7ce7dbc3ac4b4a832d2dbff3e378ad866f
Michelle Yi <m7890yi@gmail.com>
2020-10-01 16:29:08 -0400

    finished phase 1

--- storage-project.js
+++ storage-project.js
@@ -1,11 +1,18 @@
+const storeItem = () => {
+  let selectValue = document.getElementById("items").value;
+  let quantity = document.getElementById("quantity").value;
+  localStorage.setItem(selectValue, quantity);
+};
 window.addEventListener("DOMContentLoaded", (event) => {
-  // const storeItem = () => {
+  let form = document.getElementsByClassName("form");
+  form[0].addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  // };
+    storeItem();
+  });
 
   // const showCart = () => {
-  jahsgvaeilrugvi;
-  ASUreh;
+
   // };
 
   // const removeItem = () => {
