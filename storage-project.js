4061fc8147d7088efad8317acc819009df58954f
Gabriel <gabriel.gutz@live.com>
2020-10-01 17:25:25 -0400

    Phase 1 complete

--- storage-project.js
+++ storage-project.js
@@ -3,17 +3,33 @@ const storeItem = () => {
   let quantity = document.getElementById("quantity").value;
   localStorage.setItem(selectValue, quantity);
 };
+
+const showCart = () => {
+  const cart = document.getElementById("shopping-cart");
+
+  for (i = 0; i < localStorage.length; i++){
+    const key = localStorage.key(i);
+
+    const value = localStorage.getItem(key)
+
+    let shoppingCart = document.createElement("div");
+
+    shoppingCart.innerHTML = key + ": " + value;
+
+    cart.appendChild(shoppingCart);
+  }
+};
+
 window.addEventListener("DOMContentLoaded", (event) => {
   let form = document.getElementsByClassName("form");
   form[0].addEventListener("submit", (event) => {
     event.preventDefault();
 
     storeItem();
+    showCart();
   });
 
-  // const showCart = () => {
 
-  // };
 
   // const removeItem = () => {
 
